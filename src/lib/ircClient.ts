6d46511243d41c432123747191e0b79e6c36a477
Valerie Liu <v.a.pond@outlook.com>
2025-05-12 12:27:53 +0100

    Keep parsed ISUPPORT and work towards properly parsing MODE

--- src/lib/ircClient.ts
+++ src/lib/ircClient.ts
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import type { Channel, Server, User, ParsedValue } from "../types";
+import type { Channel, ParsedValue, Server, User } from "../types";
 import {
   parseIsupport,
   parseMessageTags,
